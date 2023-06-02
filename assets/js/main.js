e3af5afe0a37fd81d819700f6215c1559b46cced
iamharm <iamharm@gmail.com>
2023-06-02 10:17:12 +0200

    Fixed Slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,69 +1,36 @@
-$(document).ready(function(){
-    
-    var videoObject = $('.modal-content');
-
-    $('.video-trigger').on('click', function(){
-      videoObject.attr('src', $(this).data('video'));
-      $('.iframe-pop').addClass('iframe-visible');
-    });
-  
-     $('.modal-close').on('click', function(){
-      videoObject.attr('src', '');
-      $('.iframe-pop').removeClass('iframe-visible');
-    });
-  
-    $('.iframe-pop').on('click', function(){
-      videoObject.attr('src', '');
-      $('.iframe-pop').removeClass('iframe-visible');
-    });
-  
-});
-
-jQuery(document).ready(function($) {
-
-  $('.ds-slider').slick({
-	    dots: false,
-	    infinite: true,
-	    arrows: false,
-	    speed: 600,
-	    centerMode: true,
-	  	slidesToShow: 4,
-	  	prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='ri-arrow-left-line' ></i></button>",
-	  	nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='ri-arrow-right-line'></i></button>",
-	  	responsive: [
-			{
-	  	      breakpoint: 1280,
-	  	      settings: {
-	  	        slidesToShow: 4,
-	  	        infinite: true,
-	  	        dots: true
-	  	      }
-	  	    },
-			{
-	  	      breakpoint: 1024,
-	  	      settings: {
-	  	        slidesToShow: 3,
-	  	        infinite: true,
-	  	        dots: true
-	  	      }
-	  	    },
-	  	    {
-	  	      breakpoint: 768,
-	  	      settings: {
-	  	        slidesToShow: 1,
-	  	      }
-	  	    },
-	  	    {
-	  	      breakpoint: 480,
-	  	      settings: {
-	  	        slidesToShow: 1,
-	  	      }
-	  	    }
-	  	    // You can unslick at a given breakpoint now by adding:
-	  	    // settings: "unslick"
-	  	    // instead of a settings object
-	  	  ]
+$('.ds-slider').slick({
+	dots: false,
+	infinite: true,
+	speed: 600,
+	arrows: false,
+	slidesToShow: 4,
+	slidesToScroll: 4,
+	responsive: [
+	  {
+		breakpoint: 1024,
+		settings: {
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  infinite: true,
+		  dots: false
+		}
+	  },
+	  {
+		breakpoint: 600,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+	  // You can unslick at a given breakpoint now by adding:
+	  // settings: "unslick"
+	  // instead of a settings object
+	]
   });
-   
-
-});
