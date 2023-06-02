7cd25adaae5e44e72903bfa07edc35ea1f3ce2ac
iamharm <iamharm@gmail.com>
2023-06-02 11:37:38 +0200

    Bug Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,8 @@ $('.ds-slider').slick({
 	  {
 		breakpoint: 1024,
 		settings: {
-		  slidesToShow: 3,
-		  slidesToScroll: 3,
+		  slidesToShow: 2,
+		  slidesToScroll: 2,
 		  infinite: true,
 		  dots: false
 		}
